371f3f2bb1b9f3c73c4d31635e6ab7fcd7eb7919
Giohaitaylen <phanhongquan_t66@hus.edu.vn>
2024-12-29 18:57:58 +0700

    Update mqtt.h

--- src/secrets/mqtt.h
+++ src/secrets/mqtt.h
@@ -1,7 +1,7 @@
 namespace MQTT
 {
     const char *username = "giohaitaylen";
-    const char *password = "quan2762003";
+    const char *password = "Quan2762003";
     const char *broker = "0003f0f014c248ef859fcb26c79c96fd.s1.eu.hivemq.cloud";
     const char* mqtt_topic_pub = "home/lightSensor";
     const char* mqtt_topic_sub = "home/ledControl";
